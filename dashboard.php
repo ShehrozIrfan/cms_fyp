95f2897e288fd1092ad5c7b5df26945f36484b15
shehrozirfan <shehrozirfan89@gmail.com>
2021-10-03 13:43:54 +0500

    Dashboard file fixed

--- dashboard.php
+++ dashboard.php
@@ -21,22 +21,34 @@ if(!isset($_SESSION['login_user'])){
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .container-custom {
+            width: 80%;
+            margin-left: auto;
+            margin-right: 50px;
+        }
+        @media(max-width: 992px) {
+            .container-custom {
+                width: 90%;
+                margin-left: auto;
+                margin-right: auto;
+            }
+        }
+    </style>
 </head>
 <body>
     <!-- header -->
     <?php include 'header.php' ?> <!-- header ends -->
     <div class="pd_top"></div>
-    <div class="container">
-
-    <div class="row justify-content-center mt-5 mb-3">
-        <div class="col-md-12 text-center">
-            <div class="jumbotron">
-                <h1>Welcome to CMS</h1>
-                <p>This is the welcome page for CMS</p>
+    <div class="container-custom">
+        <div class="row justify-content-center mt-5 mb-3">
+            <div class="col-md-12 text-center">
+                <div class="jumbotron">
+                    <h1>Welcome to CMS</h1>
+                    <p>This is the welcome page for CMS, Govt. Islamia Graduate College, Civil Lines, Lahore. You can manage Accounts, Admissions, Attendances, Examinations, Hostel from this single platform.</p>
+                </div>
             </div>
         </div>
-    </div>
-
     </div>
 
     <script src="app.js"></script>
